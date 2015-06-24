aa1b4a324724d064fc5d1d670f58e358b8ef1700
jfisher1995 <jfisher1995@gmail.com>
2015-06-24 09:40:33 -0600

    Update newScript.js

--- newScript.js
+++ newScript.js
@@ -1,22 +1,19 @@
 var entry = [],
 	right = ["articleRight", "articleMiddle", "articleLeft"],
     combo = [],
-    winnerText = $('.notify');
+    notes = [],
+    winnerText = $(".notify");
 
 winnerText.hide();
 
-$('article').click(function(e){
+$("article").click(function(e){
 	var chk = $(this).hasClass("sunk"),
 	    ths = $(this),
 	    cls = ths.attr("class");
 	if ( !chk ) {
 		ths.addClass("sunk");
-	} else {
-		ths.removeClass("sunk");
 	}
 	combo.plus(cls);
-	console.log(entry);
-	console.log("winner: \r" + winner);
 });
 
 combo.plus = function(t) {
@@ -24,20 +21,31 @@ combo.plus = function(t) {
 	entry.push(t);
 	if (entry.length > 2) {
 		combo.check();
-		$('.notepad').append("<p>" + entry + "</p>");
+		//update notepad
+		$(".notepad").append("<p>" + entry + "</p>");
+		if ($(".notepad p").length > 5){
+			notes.rem();
+		}
+		//clear for new game
+		$("article").removeClass("sunk");
 		entry.length = 0;
 	}
 }
+
 combo.check = function() {
 	//  validate the combo or reset the array 
 	for (i=0; i<3; i++){
 		if (entry[i] != right[i]){
 			winnerText.hide();
-			$('.notepad').show();
+			$(".notepad").show();
 			return;
 		}
 	}
 	winner = true;
 	winnerText.show();
-	$('.notepad').hide();
+	$(".notepad").hide();
+}
+
+notes.rem = function(){
+	$(".notepad p:nth-child(2)").remove();
 }
